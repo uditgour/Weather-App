484d73a6010828f207dbe7682bcbc85b7e49dca0
udit gour <gourudit1@gmail.com>
2021-06-26 10:21:18 +0530

    first commit

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.render("index");
